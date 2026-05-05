33b5b360a3f5db2643ed1e5232b81e4ceb3056b6
Natdanai Hongsanai <45287293+akualia@users.noreply.github.com>
2026-05-05 17:39:35 +0700

    create repo

---
+++ GameTracker/Models/Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using SQLite;
+
+namespace GameTracker.Models
+{
+    public class Game
+    {
+        // --- ข้อมูลที่ได้มาจาก RAWG API ---
+
+        [PrimaryKey]
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("background_image")]
+        public string BackgroundImage { get; set; }
+
+        [JsonPropertyName("rating")]
+        public double Rating { get; set; }
+
+        // --- ข้อมูลสำหรับบันทึกในแอปของเราเอง (Local Data) ---
+
+        public string Platform { get; set; }
+        public int Progress { get; set; }
+        public string PlayStatus { get; set; }
+        public bool IsFavorite { get; set; }
+    }
+
+    public class GameResponse
+    {
+        [JsonPropertyName("results")]
+        public List<Game> Results { get; set; }
+    }
+}
